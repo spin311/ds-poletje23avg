fabe1e6bacc0dd9dc2452479cb289ae2fe70ce42
spin311 <79504899+spin311@users.noreply.github.com>
2023-08-18 12:04:06 +0000

    miha html

---
+++ ucenci/miha/kokoloko/kokoloko.js
@@ -0,0 +1,10 @@
+function spremeni_barvo(barva){
+	document.getElementById("ozadje").style.backgroundColor = barva;
+	}
+	function barva (_barva_){
+document.getElementById("krnek").style.textColor = _barva_;
+
+
+	}
+
+
