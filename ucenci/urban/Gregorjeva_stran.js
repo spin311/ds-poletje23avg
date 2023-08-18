5db739a12a8dbb13b98ca4b2291cc401a82e9c50
spin311 <79504899+spin311@users.noreply.github.com>
2023-08-18 11:26:43 +0000

    ucenci + urban

---
+++ ucenci/urban/Gregorjeva_stran.js
@@ -0,0 +1,6 @@
+alert("Go to the ECKIart site for videos. You Can also find the link in the bottom of the page!")
+
+function imeFunkcije(id) {
+	document.getElementById(id).style.color = "red";
+	document.getElementById(id).innerHTML = "test";
+}
